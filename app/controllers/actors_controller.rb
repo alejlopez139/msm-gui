4f0df80dc34ce446e5d3b041d9d37b4e2f62efbd
Alejandro Lopez <33030244+alejlopez139@users.noreply.github.com>
2023-10-05 16:07:36 +0000

    passed all tests

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,5 +1,41 @@
 class ActorsController < ApplicationController
 
+  def create
+    a = Actor.new
+    a.name = params.fetch("query_name")
+    a.dob = params.fetch("query_dob")
+    a.bio = params.fetch("query_bio")
+    a.image = params.fetch("query_image")
+
+    a.save
+
+    redirect_to("/actors")
+
+  end
+
+  def destroy
+    the_id = params.fetch("an_id")
+    matching_records = Actor.where({:id => the_id})
+    the_actor = matching_records.at(0)
+    the_actor.destroy
+
+    redirect_to("/actors")
+end
+
+  def update
+    a_id = params.fetch("the_id")
+    matching_records = Actor.where({:id => a_id})
+    the_actor = matching_records.at(0)
+
+    the_actor.name = params.fetch("query_name")
+    the_actor.dob = params.fetch("query_dob")
+    the_actor.bio = params.fetch("query_bio")
+    the_actor.image = params.fetch("query_image")
+
+    the_actor.save
+
+    redirect_to("/actors/#{the_actor.id}")
+  end
 
   def index
     matching_actors = Actor.all
